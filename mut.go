7718505dd108ddcd18c97cfbe7cc502dd8c652d7
Reynaldo Endis <reynaldo.endis@gmail.com>
2022-07-12 11:57:59 -0400

    survey crud

--- mut.go
+++ mut.go
@@ -2,18 +2,62 @@ package surveygo
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/rendis/surveygo/part"
 )
 
-//func AddSubQuestion(survey *Survey, nameId, questionJson string) error {
-//
-//}
+func (s *Survey) RemoveQuestion(nameId string) error {
+	var ins internalSurvey
+	err := json.Unmarshal([]byte(s.JsonSurvey), &ins)
+	if err != nil {
+		return err
+	}
 
-func AddQuestion(survey *Survey, questionJson string) error {
-	return AddQuestionBytes(survey, []byte(questionJson))
+	var found bool
+	for i, q := range ins.Questions {
+		if *q.NameId == nameId {
+			ins.Questions = append(ins.Questions[:i], ins.Questions[i+1:]...)
+			found = true
+			break
+		}
+	}
+	if !found {
+		return fmt.Errorf("question '%s' not found", nameId)
+	}
+
+	return s.internalUpdate(&ins)
 }
 
-func AddQuestionBytes(survey *Survey, questionByte []byte) error {
+func (s *Survey) AddQuestion(questionJson string) error {
+	return s.AddQuestionBytes([]byte(questionJson))
+}
+
+func (s *Survey) AddQuestionBytes(questionByte []byte) error {
+	var question part.Question
+	err := json.Unmarshal(questionByte, &question)
+	if err != nil {
+		return err
+	}
+
+	if _, ok := s.NameIdPaths[*question.NameId]; ok {
+		return fmt.Errorf("question '%s' already exists", *question.NameId)
+	}
+
+	var ins internalSurvey
+	err = json.Unmarshal([]byte(s.JsonSurvey), &ins)
+	if err != nil {
+		return err
+	}
+
+	ins.Questions = append(ins.Questions, question)
+	return s.internalUpdate(&ins)
+}
+
+func (s *Survey) UpdateQuestion(questionJson string) error {
+	return s.UpdateQuestionBytes([]byte(questionJson))
+}
+
+func (s *Survey) UpdateQuestionBytes(questionByte []byte) error {
 	var question part.Question
 	err := json.Unmarshal(questionByte, &question)
 	if err != nil {
@@ -21,12 +65,27 @@ func AddQuestionBytes(survey *Survey, questionByte []byte) error {
 	}
 
 	var ins internalSurvey
-	err = json.Unmarshal([]byte(survey.JsonSurvey), &ins)
+	err = json.Unmarshal([]byte(s.JsonSurvey), &ins)
 	if err != nil {
 		return err
 	}
-	ins.Questions = append(ins.Questions, question)
 
+	var found bool
+	for i, q := range ins.Questions {
+		if *q.NameId == *question.NameId {
+			ins.Questions[i] = question
+			found = true
+			break
+		}
+	}
+	if !found {
+		return fmt.Errorf("question '%s' not found", *question.NameId)
+	}
+
+	return s.internalUpdate(&ins)
+}
+
+func (s *Survey) internalUpdate(ins *internalSurvey) error {
 	paths, err := ins.getNameIdPaths()
 	if err != nil {
 		return err
@@ -36,7 +95,7 @@ func AddQuestionBytes(survey *Survey, questionByte []byte) error {
 	if err != nil {
 		return err
 	}
-	survey.JsonSurvey = string(b)
-	survey.NameIdPaths = paths
+	s.JsonSurvey = string(b)
+	s.NameIdPaths = paths
 	return nil
 }
